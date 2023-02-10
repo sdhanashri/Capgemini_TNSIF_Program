35e0743c488347a0e300bacb7c7d80d736a0e39d
satya <satya@LAPTOP-H5KTAQQ0>
2023-02-10 13:28:45 +0530

    hello

---
+++ true false/src/day/tr1.java
@@ -0,0 +1,26 @@
+package day;
+
+public class tr1 {
+
+	public static void main(String[] args) {
+		
+		String d="maharashtra";
+		System.out.println(d.endsWith("tra"));
+		
+		System.out.println(d.endsWith("ta"));
+		
+		System.out.println(d.endsWith("a"));
+		
+		System.out.println(d.endsWith("s"));
+		
+		String s="ranjangaon";
+		System.out.println(s);
+		
+		System.out.println(s.startsWith("mha"));
+		System.out.println(s.startsWith("r"));
+		System.out.println(s.startsWith("ran"));
+		// TODO Auto-generated method stub
+
+	}
+
+}
