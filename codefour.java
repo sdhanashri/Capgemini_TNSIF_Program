eb8e12398a72b39f83f7721b4c78bda6ead2a892
sdhanashri <124771610+sdhanashri@users.noreply.github.com>
2023-02-10 14:07:14 +0530

    Add files via upload

---
+++ codefour.java
@@ -0,0 +1,17 @@
+package day5;
+
+public class codefour {
+	
+	int a=45;
+	String r="narika";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	codefour b=new codefour();
+	
+	System.out.println(b.a);
+	System.out.println(b.r);
+
+	}
+
+}
